f39dc0b627be29f9cc1904397759f858d2254fea
Jacek Kosiński <kashiash@gmail.com>
2023-01-24 00:36:27 +0100

    Initial Commit

---
+++ Bluprints/BluprintsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluprintsApp.swift
+//  Bluprints
+//
+//  Created by Jacek Kosinski U on 24/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct BluprintsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
